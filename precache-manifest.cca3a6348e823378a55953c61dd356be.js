789b5f893fe15f92aef2ed1e30a541c0728be66b
DipuMsd7 <dsarode013@gmail.com>
2021-08-18 15:24:55 +0530

    Updates

---
+++ precache-manifest.cca3a6348e823378a55953c61dd356be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ce519727a91a353af2b7fc322b75a9c",
+    "url": "/MovieDB/index.html"
+  },
+  {
+    "revision": "faf46aa1a1d52d2e6df6",
+    "url": "/MovieDB/static/css/main.8ccfd6f5.chunk.css"
+  },
+  {
+    "revision": "a8a24e888dc5f809e860",
+    "url": "/MovieDB/static/js/2.4c8bc927.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/MovieDB/static/js/2.4c8bc927.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faf46aa1a1d52d2e6df6",
+    "url": "/MovieDB/static/js/main.3035d086.chunk.js"
+  },
+  {
+    "revision": "c15a3f243b010abf760f",
+    "url": "/MovieDB/static/js/runtime-main.ae4ae549.js"
+  }
+]);
